739c95272a124ecfa1129abb6914f1afb11ff1b6
liufeihua <1002219331@qq.com>
2021-03-11 20:54:32 +0800

    可选参数

--- api/internal/types/types.go
+++ api/internal/types/types.go
@@ -2604,9 +2604,9 @@ type AddCouponResp struct {
 }
 
 type AddDeptReq struct {
-	Name     string `json:"name"`      // 机构名称
-	ParentId int64  `json:"parent_id"` // 上级机构ID，一级机构为0
-	OrderNum int64  `json:"order_num"` // 排序
+	Name     string `json:"name"`               // 机构名称
+	ParentId int64  `json:"parent_id,optional"` // 上级机构ID，一级机构为0
+	OrderNum int64  `json:"order_num"`          // 排序
 }
 
 type AddDeptResp struct {
@@ -2943,7 +2943,7 @@ type AddMemberTaskResp struct {
 
 type AddMenuReq struct {
 	Name         string `json:"name"`                   // 菜单名称
-	ParentId     int64  `json:"parent_id"`              // 父菜单ID，一级菜单为0
+	ParentId     int64  `json:"parent_id,optional"`     // 父菜单ID，一级菜单为0
 	Url          string `json:"url,optional"`           // 菜单URL,类型：1.普通页面（如用户管理， /sysmodel/user） 2.嵌套完整外部页面，以http(s)开头的链接 3.嵌套服务器页面，使用iframe:前缀+目标URL(如SQL监控， iframe:/druid/login.html, iframe:前缀会替换成服务器地址)
 	Perms        string `json:"perms,optional"`         // 授权(多个用逗号分隔，如：sysmodel:user:add,sysmodel:user:edit)
 	Type         int64  `json:"type,optional"`          // 类型   0：目录   1：菜单   2：按钮
