6fb41bfba38253615138bcecef093ebd4db38eb0
koobe <koobe.liu@uaf.com.cn>
2021-03-17 18:18:09 +0800

    update sql

--- rpc/doc/sql/gozero_pms_member_price.sql
+++ rpc/doc/sql/gozero_pms_member_price.sql
@@ -13,4 +13,4 @@ create table pms_member_price
 INSERT INTO gozero.pms_member_price (id, product_id, member_level_id, member_price, member_level_name) VALUES (112, 23, 1, 88.00, '黄金会员');
 INSERT INTO gozero.pms_member_price (id, product_id, member_level_id, member_price, member_level_name) VALUES (113, 23, 2, 88.00, '白金会员');
 INSERT INTO gozero.pms_member_price (id, product_id, member_level_id, member_price, member_level_name) VALUES (114, 23, 3, 66.00, '钻石会员');
-INSERT INTO gozero.pms_member_price (id, product_id, member_level_id, member_price, member_level_name) VALUES (142, 31, 1, ' ', '黄金会员');
+INSERT INTO gozero.pms_member_price (id, product_id, member_level_id, member_price, member_level_name) VALUES (142, 31, 1, 66.00, '黄金会员');
