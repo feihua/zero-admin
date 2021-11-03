aa211be45f0acccc01519a16be87cd485d6e6803
liufeihua <1002219331@qq.com>
2021-11-03 20:14:53 +0800

    更新权限过滤

--- api/internal/middleware/checkurlmiddleware.go
+++ api/internal/middleware/checkurlmiddleware.go
@@ -30,37 +30,43 @@ func (m *CheckUrlMiddleware) Handle(next http.HandlerFunc) http.HandlerFunc {
 			return
 		}
 
-		//获取用户能访问的url
-		urls, err := m.Redis.Get(userId)
-		if err != nil {
-			logx.Errorf("用户：%s,获取redis连接异常", userId)
-			httpx.Error(w, errorx.NewDefaultError(fmt.Sprintf("用户：%s,获取redis连接异常", userId)))
-			return
-		}
-
-		if len(strings.TrimSpace(urls)) == 0 {
-			logx.Errorf("用户userId: %s,还没有登录", userId)
-			httpx.Error(w, errorx.NewDefaultError(fmt.Sprintf("用户userId: %s,还没有登录,请先登录", userId)))
-			return
-		}
-
-		backUrls := strings.Split(urls, ",")
-
-		b := false
-		for _, url := range backUrls {
-			if url == r.RequestURI {
-				b = true
-				break
-			}
-		}
-
-		if b || r.RequestURI == "/api/sys/user/currentUser" || r.RequestURI == "/api/sys/user/selectAllData" || r.RequestURI == "/api/sys/role/queryMenuByRoleId" {
+		if r.RequestURI == "/api/sys/user/currentUser" || r.RequestURI == "/api/sys/user/selectAllData" || r.RequestURI == "/api/sys/role/queryMenuByRoleId" {
 			logx.Infof("用户userId: %s,访问: %s路径", userId, r.RequestURI)
 			next(w, r)
 		} else {
-			logx.Errorf("用户userId: %s,没有访问: %s路径的权限", userId, r.RequestURI)
-			httpx.Error(w, errorx.NewDefaultError(fmt.Sprintf("用户userId: %s,没有访问: %s,路径的的权限,请联系管理员", userId, r.RequestURI)))
-			return
+			//获取用户能访问的url
+			urls, err := m.Redis.Get(userId)
+			if err != nil {
+				logx.Errorf("用户：%s,获取redis连接异常", userId)
+				httpx.Error(w, errorx.NewDefaultError(fmt.Sprintf("用户：%s,获取redis连接异常", userId)))
+				return
+			}
+
+			if len(strings.TrimSpace(urls)) == 0 {
+				logx.Errorf("用户userId: %s,还没有登录", userId)
+				httpx.Error(w, errorx.NewDefaultError(fmt.Sprintf("用户userId: %s,还没有登录,请先登录", userId)))
+				return
+			}
+
+			backUrls := strings.Split(urls, ",")
+
+			b := false
+			for _, url := range backUrls {
+				if url == r.RequestURI {
+					b = true
+					break
+				}
+			}
+
+			if b {
+				logx.Infof("用户userId: %s,访问: %s路径", userId, r.RequestURI)
+				next(w, r)
+			} else {
+				logx.Errorf("用户userId: %s,没有访问: %s路径的权限", userId, r.RequestURI)
+				httpx.Error(w, errorx.NewDefaultError(fmt.Sprintf("用户userId: %s,没有访问: %s,路径的的权限,请联系管理员", userId, r.RequestURI)))
+				return
+			}
+
 		}
 
 	}
