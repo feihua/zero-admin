a71ecd81cce55e00474513de4b2a16516f46c4f4
koobe <koobe.liu@uaf.com.cn>
2023-04-20 15:31:46 +0800

    更新参数

--- rpc/sms/internal/logic/flashpromotionlistlogic.go
+++ rpc/sms/internal/logic/flashpromotionlistlogic.go
@@ -25,7 +25,7 @@ func NewFlashPromotionListLogic(ctx context.Context, svcCtx *svc.ServiceContext)
 
 func (l *FlashPromotionListLogic) FlashPromotionList(in *sms.FlashPromotionListReq) (*sms.FlashPromotionListResp, error) {
 	all, err := l.svcCtx.SmsFlashPromotionModel.FindAll(l.ctx, in.Title, in.Status, in.Current, in.PageSize)
-	count, _ := l.svcCtx.SmsFlashPromotionModel.Count(l.ctx)
+	count, _ := l.svcCtx.SmsFlashPromotionModel.Count(l.ctx, in.Title, in.Status)
 
 	if err != nil {
 		reqStr, _ := json.Marshal(in)
