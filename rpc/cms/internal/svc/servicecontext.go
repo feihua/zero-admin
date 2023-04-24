bde45f24861d5abfdb5fbd3a2d9bae2101b091c8
koobe <koobe.liu@uaf.com.cn>
2023-04-24 17:34:58 +0800

    cms数据库映射

--- rpc/cms/internal/svc/servicecontext.go
+++ rpc/cms/internal/svc/servicecontext.go
@@ -1,13 +1,43 @@
 package svc
 
-import "zero-admin/rpc/cms/internal/config"
+import (
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+	"zero-admin/rpc/cms/internal/config"
+	"zero-admin/rpc/model/cmsmodel"
+)
 
 type ServiceContext struct {
 	Config config.Config
+
+	CmsHelpModel                         cmsmodel.CmsHelpModel
+	CmsHelpCategoryModel                 cmsmodel.CmsHelpCategoryModel
+	CmsMemberReportModel                 cmsmodel.CmsMemberReportModel
+	CmsPrefrenceAreaModel                cmsmodel.CmsPrefrenceAreaModel
+	CmsPrefrenceAreaProductRelationModel cmsmodel.CmsPrefrenceAreaProductRelationModel
+	CmsSubjectModel                      cmsmodel.CmsSubjectModel
+	CmsSubjectCategoryModel              cmsmodel.CmsSubjectCategoryModel
+	CmsSubjectCommentModel               cmsmodel.CmsSubjectCommentModel
+	CmsSubjectProductRelationModel       cmsmodel.CmsSubjectProductRelationModel
+	CmsTopicModel                        cmsmodel.CmsTopicModel
+	CmsTopicCategoryModel                cmsmodel.CmsTopicCategoryModel
+	CmsTopicCommentModel                 cmsmodel.CmsTopicCommentModel
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
+	sqlConn := sqlx.NewMysql(c.Mysql.Datasource)
 	return &ServiceContext{
-		Config: c,
+		Config:                               c,
+		CmsHelpModel:                         cmsmodel.NewCmsHelpModel(sqlConn),
+		CmsHelpCategoryModel:                 cmsmodel.NewCmsHelpCategoryModel(sqlConn),
+		CmsMemberReportModel:                 cmsmodel.NewCmsMemberReportModel(sqlConn),
+		CmsPrefrenceAreaModel:                cmsmodel.NewCmsPrefrenceAreaModel(sqlConn),
+		CmsPrefrenceAreaProductRelationModel: cmsmodel.NewCmsPrefrenceAreaProductRelationModel(sqlConn),
+		CmsSubjectModel:                      cmsmodel.NewCmsSubjectModel(sqlConn),
+		CmsSubjectCategoryModel:              cmsmodel.NewCmsSubjectCategoryModel(sqlConn),
+		CmsSubjectCommentModel:               cmsmodel.NewCmsSubjectCommentModel(sqlConn),
+		CmsSubjectProductRelationModel:       cmsmodel.NewCmsSubjectProductRelationModel(sqlConn),
+		CmsTopicModel:                        cmsmodel.NewCmsTopicModel(sqlConn),
+		CmsTopicCategoryModel:                cmsmodel.NewCmsTopicCategoryModel(sqlConn),
+		CmsTopicCommentModel:                 cmsmodel.NewCmsTopicCommentModel(sqlConn),
 	}
 }
