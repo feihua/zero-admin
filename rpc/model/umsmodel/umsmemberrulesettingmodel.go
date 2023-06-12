5b20557319e1f66b548ea84f53b59a6bf5ecdef0
koobe <koobe.liu@uaf.com.cn>
2023-06-12 10:04:30 +0800

    批量删除

--- rpc/model/umsmodel/umsmemberrulesettingmodel.go
+++ rpc/model/umsmodel/umsmemberrulesettingmodel.go
@@ -64,7 +64,22 @@ func (m *customUmsMemberRuleSettingModel) Count(ctx context.Context) (int64, err
 }
 
 func (m *customUmsMemberRuleSettingModel) DeleteByIds(ctx context.Context, ids []int64) error {
-	query := fmt.Sprintf("delete from %s where `id` in (?)", m.table)
-	_, err := m.conn.ExecCtx(ctx, query, strings.Replace(strings.Trim(fmt.Sprint(ids), "[]"), " ", ",", -1))
+	// 拼接占位符 "?"
+	placeholders := make([]string, len(ids))
+	for i := range ids {
+		placeholders[i] = "?"
+	}
+
+	// 构建删除语句
+	query := fmt.Sprintf("DELETE FROM %s WHERE id IN (%s)", m.table, strings.Join(placeholders, ","))
+
+	// 构建参数列表
+	args := make([]interface{}, len(ids))
+	for i, id := range ids {
+		args[i] = id
+	}
+
+	// 执行删除语句
+	_, err := m.conn.ExecCtx(ctx, query, args...)
 	return err
 }
