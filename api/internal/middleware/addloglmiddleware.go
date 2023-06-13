d3ef1930adc77fa761a5de4a6788a2a1d56d2583
koobe <koobe.liu@uaf.com.cn>
2023-06-13 09:47:42 +0800

    参考chatgpt实现操作日志记录请求参数

--- api/internal/middleware/addloglmiddleware.go
+++ api/internal/middleware/addloglmiddleware.go
@@ -1,7 +1,10 @@
 package middleware
 
 import (
+	"bytes"
+	"github.com/zeromicro/go-zero/core/logx"
 	"github.com/zeromicro/go-zero/rest/httpx"
+	"io/ioutil"
 	"net/http"
 	"time"
 	"zero-admin/rpc/sys/sys"
@@ -16,6 +19,7 @@ func NewAddLogMiddleware(Sys sys.Sys) *AddLogMiddleware {
 	return &AddLogMiddleware{Sys: Sys}
 }
 
+// Handle 参考chatgpt实现
 func (m *AddLogMiddleware) Handle(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
@@ -26,29 +30,42 @@ func (m *AddLogMiddleware) Handle(next http.HandlerFunc) http.HandlerFunc {
 
 		userName := r.Context().Value("userName").(string)
 
-		// 解析 JSON 参数
-		//var requestBody map[string]interface{}
-		//err := httpx.ParseJsonBody(r, &requestBody)
-		//req, _ := json.Marshal(requestBody)
-		//logx.WithContext(r.Context()).Infov(string(req))
+		startTime := time.Now()
+
+		// 读取请求主体
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			logx.Errorf("Failed to read request body: %v", err)
+		}
+
+		// 创建一个新的请求主体用于后续读取
+		r.Body = ioutil.NopCloser(bytes.NewBuffer(body))
+
+		// 打印请求参数日志
+		logx.Infof("Request: %s %s %s", r.Method, r.RequestURI, body)
+
+		// 继续处理请求
+		next(w, r)
+
+		// 记录响应日志
+		//response := httpx.Response{ResponseWriter: w}
+		//responseData, err := response.DumpBody()
 		//if err != nil {
-		//	logx.Errorf("Failed to parse JSON body: %v", err)
-		//	httpx.Error(w, errorx.NewDefaultError("Invalid JSON body"))
-		//	return
+		//	logx.Errorf("Failed to read response body: %v", err)
 		//}
 
-		//all, _ := io.ReadAll(r.Body)
-		startTime := time.Now()
-		next(w, r)
-		endTime := time.Now()
-		duration := endTime.Sub(startTime)
-		// 添加操作日志
+		//// 打印响应日志
+		//logx.Infof("Response: %s %s %s", r.Method, r.RequestURI, responseData)
+
+		// 打印请求和响应耗时
+		duration := time.Since(startTime)
+		//// 添加操作日志
 		_, _ = m.Sys.SysLogAdd(r.Context(), &sysclient.SysLogAddReq{
 			UserName:  userName,
 			Operation: r.Method,
 			Method:    r.RequestURI,
-			Params:    "string(all)",
-			Time:      duration.Microseconds(),
+			Params:    string(body),
+			Time:      duration.Milliseconds(),
 			Ip:        httpx.GetRemoteAddr(r),
 			CreateBy:  userName,
 		})
