48cee5ea4c1326a8fd059c867f64ebe2ac158b6f
koobe <koobe.liu@uaf.com.cn>
2023-06-16 10:02:00 +0800

    fix sql

--- rpc/doc/sql/sys_user_role.sql
+++ rpc/doc/sql/sys_user_role.sql
@@ -5,5 +5,5 @@ create table sys_user_role
     user_id     bigint                              not null comment '用户ID',
     role_id     bigint                              not null comment '角色ID',
     create_by   varchar(50)                         not null comment '创建人',
-    create_time timestamp default CURRENT_TIMESTAMP not null comment '创建时间',
+    create_time timestamp default CURRENT_TIMESTAMP not null comment '创建时间'
 ) comment '用户角色';
