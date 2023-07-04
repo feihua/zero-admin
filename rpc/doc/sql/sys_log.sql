752528de7eca4ec9c36bb3afe5f63eb210f2a3dc
koobe <koobe.liu@uaf.com.cn>
2023-07-04 16:40:19 +0800

    操作日志响应参数字段长度不够

--- rpc/doc/sql/sys_log.sql
+++ rpc/doc/sql/sys_log.sql
@@ -6,10 +6,11 @@ create table sys_log
     operation       varchar(50)                         not null comment '用户操作',
     method          varchar(200)                        not null comment '请求方法',
     request_params  varchar(5000)                       not null comment '请求参数',
-    response_params varchar(5000)                       null comment '响应参数',
+    response_params text                                null comment '响应参数',
     time            bigint                              not null comment '执行时长(毫秒)',
     ip              varchar(64)                         null comment 'IP地址',
     operation_time  timestamp default CURRENT_TIMESTAMP null comment '操作时间'
 )
     comment '系统操作日志';
+
 
