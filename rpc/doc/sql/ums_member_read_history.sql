f48b89e3745c1222fe7a5d3632c98b25683a8224
koobe <koobe.liu@uaf.com.cn>
2023-07-05 18:18:10 +0800

    会员收藏记录和浏览记录建表ddl

---
+++ rpc/doc/sql/ums_member_read_history.sql
@@ -0,0 +1,16 @@
+create table ums_member_read_history
+(
+    id                bigint auto_increment comment '编号'
+        primary key,
+    member_id         bigint                              not null comment '会员id',
+    member_nick_name  varchar(50)                         not null comment '会员姓名',
+    member_icon       varchar(200)                        not null comment '会员头像',
+    product_id        bigint                              not null comment '商品id',
+    product_name      varchar(50)                         not null comment '商品名称',
+    product_pic       varchar(200)                        not null comment '商品图片',
+    product_sub_title varchar(128)                        null comment '商品标题',
+    product_price     decimal(10, 2)                      not null comment '商品价格',
+    create_time       timestamp default CURRENT_TIMESTAMP null comment '浏览时间'
+)
+    comment '用户商品浏览历史记录';
+
