6ff81cff698cd45314acce7130113a6fe3f74d6f
liufeihua <1002219331@qq.com>
2025-01-20 11:01:36 +0800

    update pass

--- rpc/ums/gen/generator.go
+++ rpc/ums/gen/generator.go
@@ -15,7 +15,7 @@ func main() {
 		FieldNullable: true,
 	})
 
-	//var dsn = "root:123456@tcp(127.0.0.1:3306)/gozero?charset=utf8mb4&parseTime=true&loc=Asia%2FShanghai"
+	//var dsn = "12341qweqfsd2356@tcp(127.0.0.1:3306)/gozero?charset=utf8mb4&parseTime=true&loc=Asia%2FShanghai"
 	var dsn = "root:oMbPi5munxCsBSsiLoPV@tcp(110.41.179.89:3306)/zero-ums?charset=utf8mb4&parseTime=true&loc=Asia%2FShanghai"
 	// Initialize a *gorm.DB instance
 	db, _ := gorm.Open(mysql.Open(dsn))
