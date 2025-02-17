12d47363ca4f288cf7ad7b18baeade8a647915e8
liufeihua <1002219331@qq.com>
2025-02-17 16:24:46 +0800

    feat(oms): add query order return reason detail functionality and update order return reason logic
    - Implement QueryOrderReturnReasonDetailLogic to fetch order return reason details
    - Update UpdateOrderReturnReasonLogic to use GORM for database operations- Add error handling and logging in both query and update functions
    - Improve code structure and readability in both files

--- rpc/oms/internal/logic/orderreturnreasonservice/queryorderreturnreasondetaillogic.go
+++ rpc/oms/internal/logic/orderreturnreasonservice/queryorderreturnreasondetaillogic.go
@@ -2,13 +2,20 @@ package orderreturnreasonservicelogic
 
 import (
 	"context"
-
+	"errors"
+	"github.com/feihua/zero-admin/pkg/time_util"
+	"github.com/feihua/zero-admin/rpc/oms/gen/query"
 	"github.com/feihua/zero-admin/rpc/oms/internal/svc"
 	"github.com/feihua/zero-admin/rpc/oms/omsclient"
-
+	"github.com/zeromicro/go-zero/core/logc"
 	"github.com/zeromicro/go-zero/core/logx"
 )
 
+// QueryOrderReturnReasonDetailLogic 查询退货原因表详情
+/*
+Author: 刘飞华
+Date: 2025/02/07 10:05:47
+*/
 type QueryOrderReturnReasonDetailLogic struct {
 	ctx    context.Context
 	svcCtx *svc.ServiceContext
@@ -23,9 +30,24 @@ func NewQueryOrderReturnReasonDetailLogic(ctx context.Context, svcCtx *svc.Servi
 	}
 }
 
-// 查询退货原因表详情
+// QueryOrderReturnReasonDetail 查询退货原因表详情
 func (l *QueryOrderReturnReasonDetailLogic) QueryOrderReturnReasonDetail(in *omsclient.QueryOrderReturnReasonDetailReq) (*omsclient.QueryOrderReturnReasonDetailResp, error) {
-	// todo: add your logic here and delete this line
+	item, err := query.OmsOrderReturnReason.WithContext(l.ctx).Where(query.OmsOrderReturnReason.ID.Eq(in.Id)).First()
 
-	return &omsclient.QueryOrderReturnReasonDetailResp{}, nil
+	if err != nil {
+		logc.Errorf(l.ctx, "查询退货原因表详情失败,参数:%+v,异常:%s", in, err.Error())
+		return nil, errors.New("查询退货原因表详情失败")
+	}
+
+	data := &omsclient.QueryOrderReturnReasonDetailResp{
+		Id:         item.ID,                              //
+		Name:       item.Name,                            // 退货类型
+		Sort:       item.Sort,                            // 排序
+		Status:     item.Status,                          // 状态：0->不启用；1->启用
+		CreateTime: time_util.TimeToStr(item.CreateTime), // 创建时间
+
+	}
+
+	logc.Infof(l.ctx, "查询退货原因表详情,参数：%+v,响应：%+v", in, data)
+	return data, nil
 }
--- rpc/oms/internal/logic/orderreturnreasonservice/updateorderreturnreasonlogic.go
+++ rpc/oms/internal/logic/orderreturnreasonservice/updateorderreturnreasonlogic.go
@@ -6,6 +6,7 @@ import (
 	"github.com/feihua/zero-admin/rpc/oms/gen/query"
 	"github.com/feihua/zero-admin/rpc/oms/internal/svc"
 	"github.com/feihua/zero-admin/rpc/oms/omsclient"
+	"time"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -28,12 +29,15 @@ func NewUpdateOrderReturnReasonLogic(ctx context.Context, svcCtx *svc.ServiceCon
 // UpdateOrderReturnReason 更新退货原因表
 func (l *UpdateOrderReturnReasonLogic) UpdateOrderReturnReason(in *omsclient.UpdateOrderReturnReasonReq) (*omsclient.UpdateOrderReturnReasonResp, error) {
 	q := query.OmsOrderReturnReason
-	_, err := q.WithContext(l.ctx).Updates(&model.OmsOrderReturnReason{
-		ID:     in.Id,     //
-		Name:   in.Name,   // 退货类型
-		Sort:   in.Sort,   // 排序
-		Status: in.Status, // 状态：0->不启用；1->启用
-	})
+	item := &model.OmsOrderReturnReason{
+		ID:         in.Id,      //
+		Name:       in.Name,    // 退货类型
+		Sort:       in.Sort,    // 排序
+		Status:     in.Status,  // 状态：0->不启用；1->启用
+		CreateTime: time.Now(), // 创建时间
+	}
+
+	err := l.svcCtx.DB.Model(&model.OmsOrderReturnReason{}).WithContext(l.ctx).Where(q.ID.Eq(in.Id)).Save(item).Error
 
 	if err != nil {
 		return nil, err
